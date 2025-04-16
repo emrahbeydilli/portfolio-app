259db4b0703e34bfc71ed59fde3668e19229f300
Emrah Beydilli <emrahbeydilli@gmail.com>
2025-04-16 15:39:51 +0300

    fixed logout notification

--- dashboard/services/logout.service.js
+++ dashboard/services/logout.service.js
@@ -1,10 +1,10 @@
 // logout.service.js
 import { loadNotificationToast } from "../components/toast.component.js";
 import { STORAGE_KEYS } from "../utils/constant.util.js";
-import { removeData } from "./storage.service.js";
+import { getData, removeData } from "./storage.service.js";
 
 const logout = () => {
-    loadNotificationToast(`Goodbye <span class="text-capitalize">${STORAGE_KEYS.CURRENTUSER}</span>. You are being directed to the login page.`, { className: "bg-success text-white", timeout: 3000 });
+    loadNotificationToast(`Goodbye <span class="text-capitalize">${getData(STORAGE_KEYS.CURRENTUSER)}</span>. You are being directed to the login page.`, { className: "bg-success text-white", timeout: 3000 });
     removeData(STORAGE_KEYS.CURRENTUSER);
     setTimeout(() => window.location.href = "../dashboard/login.html", 3000);
 };
